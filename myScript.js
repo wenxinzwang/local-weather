e28f04d0c68233ce20085cbc362a9568d8ab94e5
Wenxin Wang <wangwxzy@gmail.com>
2017-08-18 15:57:19 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,6 @@
 var tempUnit = "C";
-var temp;
+var temp, highTemp, lowTemp;
 var x = document.getElementById("location");
-var geocoder = new google.maps.Geocoder();
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -14,34 +13,33 @@ function getLocation() {
 }
 
 function callWeather(position) {
-	var url = "https://api.darksky.net/forecast/46c3ceebdd6667475c48899be8b1efb9/" 
+	var urlWeather = "https://api.darksky.net/forecast/46c3ceebdd6667475c48899be8b1efb9/" 
 	        + position.coords.latitude + "," + position.coords.longitude 
-			+ "?exclude=minutely,hourly,daily,alerts&units=si&callback=showWeacher";
-	  
-	var script = document.createElement('script');
-    script.src = url;
-  
+			+ "?exclude=minutely,hourly,alerts&units=si&callback=showWeather";//daily,
+	var scriptWeather = document.createElement('script');
+    scriptWeather.src = urlWeather;
+ 
     // after the script is loaded (and executed), remove it
-    script.onload = function () {
+    scriptWeather.onload = function () {
         this.remove();
     };
-  
+	
     // insert script tag into the DOM (append to <head>)
     var head = document.getElementsByTagName('head')[0];
-    head.insertBefore(script, null);
+    head.insertBefore(scriptWeather, null);
 	
 	codeLatLng(position.coords.latitude, position.coords.longitude);
 }
 
 function codeLatLng(lat, lng) {
-
+    var geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(lat, lng);
-	var city, country;
+	var city = "", province = "", country = "";
 	
     geocoder.geocode({'latLng': latlng}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
         console.log(results)
-            if (results[1]) {
+            if (results[0]) {
                 //formatted address
                 x.innerHTML = results[0].formatted_address;
              
@@ -56,13 +54,26 @@ function codeLatLng(lat, lng) {
                             city= results[0].address_components[i].long_name;
                         } else if(results[0].address_components[i].types[b] == "country") {
                             //this is the object you are looking for
-                            country = results[0].address_components[i].long_name;
+                            country = results[0].address_components[i].short_name;
                             break;
 						}
                     }
                 }
                 //city data
-                x.innerHTML = city + ", " + country;
+				
+				if(city) {
+                    x.innerHTML = city;
+					if(country) {
+						x.innerHTML += ", " + country;
+					}
+                } else if(province) {
+					x.innerHTML = province;
+				    if(country) {
+						x.innerHTML += ", " + country;
+					}
+				} else if(country) {
+					x.innerHTML = country;
+				}
 
             } else {
                 x.innerHTML = "No results found";
@@ -73,15 +84,19 @@ function codeLatLng(lat, lng) {
     });
 }
 
-function showWeacher(data) {
+function showWeather(data) {
 	console.log(data);
 	
-	var skycons = new Skycons({"color": "grey"});
+	var skycons = new Skycons({"color": "DarkSlateGrey"});
 	var iconW = data.currently.icon;
     temp = data.currently.temperature.toFixed(0);
+	highTemp = data.daily.data[0].apparentTemperatureMax.toFixed(0);
+	lowTemp = data.daily.data[0].apparentTemperatureMin.toFixed(0);
 	console.log(iconW);			
-	document.getElementById("temp").innerHTML = temp;
-	document.getElementById("tempUnit").innerHTML = "&#176;" + tempUnit;
+	document.getElementById("temp").innerHTML = temp + "&#176;";
+	document.getElementById("tempUnit").innerHTML =  tempUnit;
+	document.getElementById("today").innerHTML = lowTemp + " / " + highTemp + "&#176;";
+	
 	document.getElementById("summary").innerHTML = data.currently.summary;
 	
 	switch(iconW) {
@@ -126,14 +141,22 @@ function changeTempUnit() {
 	if(tempUnit === "C") {
 		tempUnit = "F";
 		temp =  temp * 9/5 + 32;
+		highTemp = highTemp * 9/5 + 32;
+		lowTemp = lowTemp * 9/5 + 32;
 	} else if(tempUnit === "F") {
 		tempUnit = "C";
 		temp = (temp - 32) * 5/9;
+		highTemp = (highTemp - 32) * 5/9;
+		lowTemp = (lowTemp - 32) * 5/9;
 	}
 	
 	temp = temp.toFixed(0);
-	document.getElementById("temp").innerHTML = temp;
-	document.getElementById("tempUnit").innerHTML = "&#176;" + tempUnit;
+	highTemp = highTemp.toFixed(0);
+	lowTemp = lowTemp.toFixed(0);
+	document.getElementById("temp").innerHTML = temp + "&#176;";
+	document.getElementById("tempUnit").innerHTML =  tempUnit;
+	document.getElementById("today").innerHTML = lowTemp + " / " + highTemp + "&#176;";
+	
 }
 
 function showError(error) {
